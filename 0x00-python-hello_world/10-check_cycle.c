419ad8239377f139e2396d772e78bf7dbf49aad2
mtmagic711 <mtmagic711@gamil.com>
2023-11-27 15:30:49 -0800

    task 10 incomplete

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: the list given.
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (1);
+}
