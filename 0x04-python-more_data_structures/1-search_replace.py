e24bef5898d392854be723b8f1946eaaa1daa314
mtmagic711 <mtmagic711@gmail.com>
2023-12-06 11:02:12 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    fun = lambda x : replace if x == search else x
-    new_list = list(map(fun, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
