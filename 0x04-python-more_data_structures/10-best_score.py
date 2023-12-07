af120f65e538ec12fa87513cbad225e8fd7b5a45
mtmagic711 <mtmagic711@gmail.com>
2023-12-07 01:12:05 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        maxim = 0
+        maxim = -1
         for key, item in a_dictionary.items():
             if item > maxim:
                 maxim = item
