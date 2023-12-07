99a6fefb2606345636192b9fefab4d330b5592d7
mtmagic711 <mtmagic711@gmail.com>
2023-12-07 01:18:06 +0100

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/pyhton3
 
 def uniq_add(my_list=[]):
-    new_list = []
-    summ = 0
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
-            summ += i
-    return summ
+    return sum(set(my_list))
