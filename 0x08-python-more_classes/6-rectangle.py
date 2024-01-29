120aa1c17b95c0ce3df7611f5c310d90b38a1cfe
mtmagic711 <mtmagic711@gmail.com>
2024-01-29 23:39:36 +0100

    population

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle():
     """the class rectangle"""
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0) -> None:
         self.width = width
         self.height = height
