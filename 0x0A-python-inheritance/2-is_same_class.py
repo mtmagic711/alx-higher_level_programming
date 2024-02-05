5342007adfbb82c696f107b45b775523e0f4104a
mtmagic711 <mtmagic711@gmail.com>
2024-02-05 13:31:11 +0100

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,4 +3,6 @@
 is exactly an instance of the specified class"""
 
 def is_same_class(obj, a_class):
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    return False
