33c8569eabe7a833a2331be21b725efbce7fb9d5
mtmagic711 <mtmagic711@gmail.com>
2024-02-05 13:35:22 +0100

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define a function that determine  if the object is exactly
+an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    return isinstance(obj, a_class)
