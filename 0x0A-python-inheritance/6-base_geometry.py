a3144480c23872dc24c5e8cd427d71adaa058497
mtmagic711 <mtmagic711@gmail.com>
2024-02-05 14:09:31 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defining a new class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """Raises an Exception."""
+
+        raise Exception("area() is not implemented")
