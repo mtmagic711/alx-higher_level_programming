97642f447b1b041e3e5e0c5cdfc2e37414f1891f
mtmagic711 <mtmagic711@gmail.com>
2024-02-05 13:57:02 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
         True or False.
     """
 
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
