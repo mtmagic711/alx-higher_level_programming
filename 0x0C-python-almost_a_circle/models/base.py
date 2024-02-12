a0d10ce52b8f0f31b06be9bc94832feabf8d5dd1
mtmagic711 <mtmagic711@gmail.com>
2024-02-12 22:55:06 +0100

    task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,6 +29,7 @@ class Base:
         filename = cls.__name__ + ".json"
         if list_objs is None:
             list_objs = []
-        json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        json_string = cls.to_json_string(
+            [obj.to_dictionary()for obj in list_objs])
         with open(filename, "w") as file:
             file.write(json_string)
