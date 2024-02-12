b4b01e21bf5ee32de7156b1d1cac1b54627a62e6
mtmagic711 <mtmagic711@gmail.com>
2024-02-12 22:30:46 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """the constructor"""
 
-        super().__init__(size, size, x=0, y=0, id=None)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Return the string representation of the Square."""
