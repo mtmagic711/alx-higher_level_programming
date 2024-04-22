b4875e16c7850ba97def76ca78781c653aa2e066
mtmagic <mtmagic711@fgmail.com>
2024-04-22 19:20:29 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const parsed = parseInt(argv[2]);
-if (!isNaN (parsed)) console.log(`My number: ${parsed}`);
+if (!isNaN(parsed)) console.log(`My number: ${parsed}`);
 else console.log('Not a number');
