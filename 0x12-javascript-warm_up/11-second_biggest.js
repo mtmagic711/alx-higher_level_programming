05732dc28bbfd9f84b72b1b386cdb2d4de4f430a
mtmagic <mtmagic711@fgmail.com>
2024-04-23 01:03:50 +0100

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const len = argv.length;
+if (len === 2 || len === 3) console.log(0);
+else {
+  argv.sort();
+  console.log(argv[len - 2]);
+}
