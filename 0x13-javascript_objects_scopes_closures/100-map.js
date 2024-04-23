a0eb2f003e3db896c55fc9510661b9a436e8dd14
mtmagic <mtmagic711@fgmail.com>
2024-04-23 22:54:54 +0100

    task 100 fixing

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+const map = list.map((x, i) => x * i);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(map);
