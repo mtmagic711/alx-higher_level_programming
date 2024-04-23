34e527afd312494d3a011267a1348fec5af7a610
mtmagic <mtmagic711@fgmail.com>
2024-04-23 21:12:40 +0100

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,10 +2,11 @@
 
 exports.esrever = function (list) {
   let j = list.length - 1;
+  let temp;
   for (let i = 0; i < j; i++, j--) {
-    let temp = list[i];
+    temp = list[i];
     list[i] = list[j];
     list[j] = temp;
   }
   return list;
-}
+};
