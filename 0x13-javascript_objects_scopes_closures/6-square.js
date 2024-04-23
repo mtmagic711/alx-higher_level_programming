bb79fd44d0b60079de30bf22d7ee4ad467e53a83
mtmagic <mtmagic711@fgmail.com>
2024-04-23 20:53:50 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const squareBase = require('./5-square.js');
+
+class Square extends squareBase {
+  charPrint (c) {
+    if (c === undefined) c = 'X';
+    for (let i = 0; i < this.height; i++) console.log(`${c}`.repeat(this.width));
+  }
+}
+
+module.exports = Square;
