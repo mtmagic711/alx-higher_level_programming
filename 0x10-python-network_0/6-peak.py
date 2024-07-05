8d8ffa78f000032772960b90b44bb1acf7a04555
mtmagic <mtmagic711@gmail.com>
2024-07-05 03:36:19 +0100

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,6 @@
+def find_peak(list_of_integers):
+    """"The function find the peak of given list."""
+    if not list_of_integers:
+        return None
+    else:
+        return max(list_of_integers)
